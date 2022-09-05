5e52767edfc067b951ed7c4795c4afa2f9ee99ee
Rousselle <ylan.rousselle76@gmail.com>
2022-09-05 16:25:56 +0200

    correct somethings from a test

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const config = []
 
 config["watcher"] = false /* true if you want to watch all error found in real time */
 config["headless"] = false /* true if you want to hidde the google chrome copy */
-config["file"] = "nws.csv" /* csv file with all error */
+config["file"] = "error.csv" /* csv file with all error */
 
 /**
  * These string will be put on regex expression
