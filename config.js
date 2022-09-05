9b736745cb84d209df5ecfce5f746f5feeb3c82d
Rousselle <ylan.rousselle76@gmail.com>
2022-09-05 11:42:28 +0200

    introduce regex exclusion word

--- config.js
+++ config.js
@@ -3,10 +3,21 @@ const config = []
 config["watcher"] = false /* true if you want to watch all error found in real time */
 config["headless"] = true /* true if you want to hidde the google chrome copy */
 config["file"] = "error.csv" /* csv file with all error */
+
 /**
- * The csv file has one column named "ERROR"
+ * These string will be put on regex expression
  */
+const exclude = [
+    "stackoverflow",
+    "php\.net",
+    "dev",
+    "forum(s)?"
+]
+
+const regexFlag = "ig"
 
 export {
-    config
+    config,
+    exclude,
+    regexFlag
 }
